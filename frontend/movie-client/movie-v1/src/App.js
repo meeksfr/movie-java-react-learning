a8617fd4c8807fb80285204d858dbc3656207200
meeksfr <mika.sustar@mail.utoronto.ca>
2024-10-21 22:00:27 -0400

    final

--- frontend/movie-client/movie-v1/src/App.js
+++ frontend/movie-client/movie-v1/src/App.js
@@ -5,9 +5,13 @@ import Layout from "./components/Layout";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Header from "./components/header/Header";
+import Trailer from "./components/trailer/Trailer";
+import Reviews from "./components/reviews/Reviews";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState([]);
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () => {
     try {
@@ -17,6 +21,15 @@ function App() {
     } catch (err) {
       console.log(err);
     }
+  };
+
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+      const singleMovie = response.data;
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviewIds);
+    } catch (error) {}
   };
 
   useEffect(() => {
@@ -29,6 +42,18 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home movies={movies} />}></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
+          <Route
+            path="/Reviews/:movieId"
+            element={
+              <Reviews
+                getMovieData={getMovieData}
+                reviews={reviews}
+                setReviews={setReviews}
+                movie={movie}
+              />
+            }
+          ></Route>
         </Route>
       </Routes>
     </div>
